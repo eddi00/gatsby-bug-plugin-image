efe0c84c718163971e5f4936f65cf3c20edd2635
ed <eduard.beliaev@gmail.com>
2022-05-22 19:20:51 +0500

    Second commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+  ],
+};
